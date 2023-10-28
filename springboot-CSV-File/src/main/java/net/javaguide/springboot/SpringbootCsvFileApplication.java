482acd0a05e20ddcc24318e59e9feb808bef9a10
sonu <sonukrsharma1299@gmail.com>
2023-10-28 20:22:45 +0530

    adding CSV files

--- springboot-CSV-File/src/main/java/net/javaguide/springboot/SpringbootCsvFileApplication.java
+++ springboot-CSV-File/src/main/java/net/javaguide/springboot/SpringbootCsvFileApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringbootCsvFileApplication {
-
+           //project URL//localhost:8081/files/upload
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootCsvFileApplication.class, args);
 
